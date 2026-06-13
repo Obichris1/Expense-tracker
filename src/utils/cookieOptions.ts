c65363f2d67e53fb9429dc0f51555fed616ad2bb
Macbook Air <macbookair@Obinnas-MacBook-Air.local>
2026-06-13 17:35:43 +0100

    cookie fix

--- src/utils/cookieOptions.ts
+++ src/utils/cookieOptions.ts
@@ -2,9 +2,9 @@ import { CookieOptions } from "express"
 
 export const cookieOptions: CookieOptions = {
     httpOnly: true,
-    secure: process.env.NODE_ENV === "production",
+    secure: process.env.NODE_ENV !== "development",
     sameSite:
-      process.env.NODE_ENV === "production"
+      process.env.NODE_ENV !== "development"
         ? "none"
         : "lax",
     maxAge: 15 * 60 * 1000,
